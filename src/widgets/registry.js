cd0f1b22e241b6089101f8960771da460f03ea5f
kturaevv <kturaevv@gmail.com>
2025-02-04 22:19:11 +0500

    fix: toggle visibility when switching renders

--- src/widgets/registry.js
+++ src/widgets/registry.js
@@ -3,15 +3,20 @@ import GUI from "./gui.js";
 export default class AppRegistry {
     constructor() {
         this.currently_running_renderer = null;
+        this.guis = [];
     }
 
     /**
      * @param {Renderer} renderer
      */
-    switch_renderer(renderer) {
+    switch_renderer(renderer, render_gui) {
         if (this.currently_running_renderer) {
             this.currently_running_renderer.terminate();
         }
+        for (const gui of this.guis) {
+            gui.make_invisible();
+        }
+        render_gui.make_visible();
         this.currently_running_renderer = renderer;
     }
 
@@ -22,16 +27,15 @@ export default class AppRegistry {
      * @param {GUI} gui
      */
     register(element, renderer_class, render_args, gui = new GUI()) {
+        this.guis.push(gui);
+
         // Handle click event
         element.addEventListener('click', async () => {
             const renderer = new renderer_class();
             await renderer.init(...render_args);
             renderer.render();
-            gui.make_visible();
-            this.switch_renderer(renderer);
+            this.switch_renderer(renderer, gui);
         });
-
-        console.log('dslfkj', gui.widgets());
 
         // Handle GUI widget events
         for (const widget of gui.widgets()) {
@@ -40,7 +44,7 @@ export default class AppRegistry {
                 const renderer = new renderer_class();
                 await renderer.init(...render_args);
                 renderer.render();
-                this.switch_renderer(renderer);
+                this.switch_renderer(renderer, gui);
             });
         }
     }
